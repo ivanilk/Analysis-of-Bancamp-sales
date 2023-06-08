ae4676e6de5841086eabcc4ae6d2a2eca9e18047
trantheluc <tranthelucbcd@gmail.com>
2023-06-08 16:04:44 +0700

    update workflows

--- flows/test.py
+++ flows/test.py
@@ -1,6 +1,6 @@
 import configparser
-import Path
+from pathlib import Path
 config = configparser.ConfigParser()
 config.read(Path('../config.ini'))
 
-print(config)
+print(config['GCP']["data_lake_bucket"])
